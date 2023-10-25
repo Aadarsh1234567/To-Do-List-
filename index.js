9981081428456ec98ea64670dc7c562cda604ed1
Aadarsh <86770825+Aadarsh1234567@users.noreply.github.com>
2023-10-25 21:12:22 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+MarkAsDone1= () => {
+  document.getElementById("task1").innerHTML = "Congrats!" 
+}
+MarkAsDone2= () => {
+    document.getElementById("task2").innerHTML = "Congrats!" 
+  }
+  MarkAsDone3= () => {
+    document.getElementById("task3").innerHTML = "Congrats!" 
+  }
+  MarkAsDone4= () => {
+    document.getElementById("task4").innerHTML = "Congrats!" 
+  }
+
